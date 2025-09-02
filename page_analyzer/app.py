039e21ea293914ef7b34cb30ffc42334d9bc32d7
alex <berezov86@gmail.com>
2025-09-02 18:05:39 +0300

    add secret

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route("/") 
 def welcome_user():
