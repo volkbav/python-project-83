cf7e94c7df031d6de04809f957fddfaae75ca079
volkbav <berezov86@gmail.com>
2025-09-02 19:46:18 +0300

    fix lint

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from flask import Flask
 
 app = Flask(__name__)
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route("/") 
 def welcome_user():
