1ce59c72bdc68a27c40a5de05c662e4414b42f39
volkbav <berezov86@gmail.com>
2025-09-04 18:03:32 +0300

    add methods for repository

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -1,7 +1,6 @@
 import os
 
 import psycopg2
-
 from psycopg2.extras import DictCursor
 
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -16,3 +15,39 @@ class UrlRepository:
         with self.conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute("SELECT * FROM urls")
             return [dict(row) for row in cur]
+        
+    def validate(self, data, current_id=None):
+        pass
+
+    def _create(self, url):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                """INSERT INTO urls (name, created_at) 
+                VALUES (%s, NOW()) RETURNING id""",
+                (url["name"],),
+            )
+            id = cur.fetchone()[0]
+            url["id"] = id
+        self.conn.commit()
+
+    def find(self, id):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+            row = cur.fetchone()
+            return dict(row) if row else None
+    
+    def _update(self, url):
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "UPDATE urls SET name = %s WHERE id = %s",
+                (url["name"], url["id"]),
+            )
+        self.conn.commit()
+
+    def get_all(self):
+        return self.get_content()
+    
+    def delete(self, id):
+        with self.conn.cursor() as cur:
+            cur.execute("DELETE FROM urls WHERE id = %s", (id,))
+        self.conn.commit()
