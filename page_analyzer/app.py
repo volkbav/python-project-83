a278d8689c0986ee5e6e0f8c4a08ff2c0f014b15
volkbav <berezov86@gmail.com>
2025-09-04 15:05:11 +0300

    fix lint

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
 
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
@@ -11,6 +11,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
+
 
 @app.route("/") 
 def index():
