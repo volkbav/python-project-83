e15f623bce9624cd667b62fae189265045a7376c
volkbav <berezov86@gmail.com>
2025-09-04 15:17:13 +0300

    apdate class UrlRepository

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -2,6 +2,8 @@ import os
 
 import psycopg2
 
+from psycopg2.extras import DictCursor
+
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 
@@ -10,4 +12,7 @@ class UrlRepository:
     def __init__(self, conn):
         self.conn = conn
 
-    
+    def get_content(self):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls")
+            return [dict(row) for row in cur]
