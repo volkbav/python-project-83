a59d2b860676153fb7dd15ee3a6198ae4da5199b
volkbav <berezov86@gmail.com>
2025-09-17 22:27:31 +0300

    step 6

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -1,59 +1,39 @@
 import requests
+from bs4 import BeautifulSoup
 
 ERROR_CODE = 500
 
 
-def get_response(url):
+def parse_content(url, url_id):
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
-        # to delete
-        print(f'responce \n{response}')
-        print(f'content \n{response.content}')
-        # --
-        return {
-            'status_code': response.status_code,
-            'content': response.content
-        }
-
     except requests.exceptions.RequestException:
-        # to delete
-        print(f'responce \n{response}')
-        print(f'content \n{response.content}')
-        # --
         return {
+            'url_id': url_id,
             'status_code': response.status_code,
-            'content': response.content
+            'h1': None,
+            'title': None,
+            'description': None
+        }
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    h1 = soup.h1.get_text(strip=True) if soup.h1 else None
+    title = soup.title.get_text(strip=True) if soup.title else None
+    description_tag = soup.find("meta", attrs={"name": "description"})
+    
+    if description_tag and description_tag.has_attr("content"):
+        description = description_tag["content"].strip()
+    else:
+        description = None
+    
+    return {
+        'url_id': url_id,
+        'status_code': response.status_code,
+        'h1': h1,
+        'title': title,
+        'description': description
         }
 
 
-# ??? for what?
-"""По идее, сначала надо получить запрос и данные по нему,
-а уже после их проверять"""
 
-
-def is_server_error(response):
-    status_code = response['status_code']
-
-    if status_code >= ERROR_CODE:
-        return False
-    else:
-        return True
-    # ???
-
-
-def parse_content(url, id):
-    response = get_response(url)
-    status_code = response['status_code']
-    """тут планирую обработать запрос
-Не забудь снять комментарий!"""
-    #    content = response['content']
-
-    data = {
-        'url_id': id,
-        'status_code': status_code,
-        'h1': None,
-        'title': None,
-        'description': None}
-
-    return data
