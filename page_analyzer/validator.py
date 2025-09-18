0f627e8a5e90e02fa99b4fd11548aad580105725
alex <berezov86@gmail.com>
2025-09-18 15:35:41 +0300

    update validator.py: add comments

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 
 
+# приведение url к нормальному виду
 def normilize_url(url):
     parsed_url = urlparse(url)
     scheme_url = parsed_url.scheme
@@ -9,14 +10,17 @@ def normilize_url(url):
     return f'{scheme_url}://{netloc_url}'
 
 
+# проверка url
 def url_validate(url):
     parsed_url = urlparse(url)
 
+    # проверка на длину url
     if not url:
         return {'name': 'URL пустой'}
     elif len(url) > 255:
         return {'name': 'URL слишком длинный'}
     
+    # проверка типа соединения
     if parsed_url.scheme not in ("http", "https") or not parsed_url.netloc:
         return {'name': 'Некорректный URL'}
     
